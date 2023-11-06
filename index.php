aa3f22666bf8a8082040e52f673a1d2c9bd1dd60
Rosa <rosalie.boulard@gmail.com>
2023-11-06 16:58:58 +0100

    creating files and codes

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets\style\style.css">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Bellota:wght@300;700&display=swap');
+        </style>
+        <title>Hackers Poulette</title> 
+    </head>
+    <body>
+        <header>
+            <?php
+                include "./views/partials/head.php";
+            ?>
+        </header>
+        <main>
+            <?php
+                include "./components/form.php";
+            ?>
+        </main>
+        <footer>
+            <?php
+                include "./views/partials/footer.php";
+            ?>
+        </footer>
+    </body>
+</html>
---
+++ views/partials/head.php
@@ -0,0 +1,6 @@
+<html>
+    <div class="head">
+        <img src="assets\hackers-poulette-logo.png">
+        <h1>Contact form</h1>
+    </div>
+</html>
